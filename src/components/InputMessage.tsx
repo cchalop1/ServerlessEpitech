c445086163aab6feb1b6c295bb8694bc8cf72043
hadrienm <adrien.heros@epitech.eu>
2022-05-23 16:24:17 +0200

    Update InputMessage.tsx

--- src/components/InputMessage.tsx
+++ src/components/InputMessage.tsx
@@ -1,34 +1,58 @@
-import React, { useState } from "react";
+import React, {useContext, useEffect, useState} from "react";
 import firebase from "../firebase/clientApp";
 
-import { collection, getFirestore } from "firebase/firestore";
+import {addDoc, collection, doc, getFirestore, serverTimestamp} from "firebase/firestore";
+import {AuthContext} from "../contexts/AuthContext";
+import {useDocument} from "react-firebase-hooks/firestore";
+import {User} from "../types/User";
 
 const db = getFirestore(firebase);
 
-const InputMessage = () => {
+type InputMessageProps = {currentConvId: string}
+
+const InputMessage = (props : InputMessageProps) => {
   const messagesRef = collection(db, "messages");
   const [messageInput, setMessageInput] = useState<string>("");
+  const authUser = useContext(AuthContext) as any;
+
+  const userDoc = doc(db, "users", authUser.user.uid);
+  const [user, loading, error] = useDocument(userDoc);
+  const [userData, setUserData] = useState<User | null>(null);
+
+  useEffect(() => {
+    if (user) {
+      setUserData(user.data() as User);
+    }
+  }, [user]);
 
   const submit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(messageInput);
+   const newMessage = {
+     content: messageInput,
+     convID: props.currentConvId,
+     createdAt: serverTimestamp(),
+     user: userData
+    }
     setMessageInput("");
-    // await addDoc(messagesRef, {
-    //     content: data,
-    //     createAt: serverTimestamp(),
-    // });
-    console.log(e);
-    // setData("");
+    await addDoc(messagesRef, newMessage);
   };
 
   return (
-    <div id="input-message">
+    <div id="input-message" className="w-9/12 absolute bottom-0 right-0 shadow-lg">
       <form onSubmit={submit}>
-        <input
-          placeholder="test"
-          value={messageInput}
-          onChange={(e) => setMessageInput(e.target.value)}
-        ></input>
+        { messageInput.length == 0 ? (
+            <input className="w-full border-2 border-grey border-solid rounded-tl-lg rounded-tr-lg rounded-br-lg rounded-bl-lg"
+                   placeholder="Enter your message"
+                   value={messageInput}
+                   onChange={(e) => setMessageInput(e.target.value)}
+            ></input>
+        ) : (
+            <input className="w-full border-2 border-blue border-solid rounded-tl-lg rounded-tr-lg rounded-br-lg rounded-bl-lg"
+                   placeholder="Enter your message"
+                   value={messageInput}
+                   onChange={(e) => setMessageInput(e.target.value)}
+            ></input>
+        )}
       </form>
     </div>
   );
