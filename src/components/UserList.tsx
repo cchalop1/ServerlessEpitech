0394c19251cf16a2834751509d524bdfdcef8d07
cchalopin <clement.chalopin@gmail.com>
2022-05-29 22:07:41 +0200

    fix: update roles in profile page

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,44 +1,66 @@
-import { collection, doc, getFirestore, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getFirestore,
+  onSnapshot,
+} from "firebase/firestore";
 import { useContext, useEffect, useState } from "react";
 import { User } from "../types/User";
 import { db } from "../firebase/clientApp";
 import UserCard from "./UserCard";
 import { AuthContext } from "../contexts/AuthContext";
 import { useDocument } from "react-firebase-hooks/firestore";
+import { Groups } from "../types/Groups";
 
 const UserList = () => {
   const authUser = (useContext(AuthContext) as any).user;
-  const usersRef = collection(db, "users");
-  const rolesRef = collection(db, "roles");
-
-  const userDoc = doc(db, "roles", authUser.uid);
-  const [userRole, loading, error] = useDocument(userDoc);
+  const usersCollection = collection(db, "users");
 
   const [users, setUsers] = useState<Array<User>>([]);
   const [isAdmin, setIsAdmin] = useState(false);
 
-  useEffect(() => {
-    onSnapshot(rolesRef, (snapshotRoles) => {
-      let roles = new Map<string, string>();
-      for (const r of snapshotRoles.docs) {
-        roles.set(r.id, r.data().value);
-      }
-      onSnapshot(usersRef, (snapshot) => {
-        const result = snapshot.docs.map((doc) => {
-          const data = doc.data();
-          return {
-            uid: doc.id,
-            role: roles.get(doc.id),
-            ...data,
-          } as User;
-        });
-        setUsers(result);
-      });
-    });
-    if (userRole) {
-      setIsAdmin(userRole.data()?.value === "admin");
+  const getRolesUsers = async (roles: "Admin" | "Manager" | "User") => {
+    const ref = doc(db, "Groups", roles);
+
+    const res = await getDoc(ref);
+    if (!res) {
+      throw Error("for fetch roles " + roles);
     }
-  }, [userRole]);
+    const data = res.data() as Groups;
+    if (!data) {
+      throw Error("for fetch data " + roles);
+    }
+    return data.users;
+  };
+
+  const findUserRole = async (userId: string) => {
+    const admis = await getRolesUsers("Admin");
+    const managers = await getRolesUsers("Manager");
+    const users = await getRolesUsers("User");
+
+    if (admis.includes(userId)) return "Admin";
+    if (managers.includes(userId)) return "Manager";
+    if (users.includes(userId)) return "User";
+    return "";
+  };
+
+  useEffect(() => {
+    onSnapshot(usersCollection, async (snapshot) => {
+      const result = snapshot.docs.map(async (doc) => {
+        const data = doc.data();
+        return {
+          uid: doc.id,
+          role: await findUserRole(doc.id),
+          ...data,
+        } as User;
+      });
+      setUsers(await Promise.all(result));
+    });
+    getRolesUsers("Admin").then((value) => {
+      setIsAdmin(value.includes(authUser.uid));
+    });
+  }, []);
 
   return (
     <div className="ml-28 w-1/3">
---
+++ src/types/Groups.ts
@@ -0,0 +1,3 @@
+export type Groups = {
+  users: Array<string>;
+};
